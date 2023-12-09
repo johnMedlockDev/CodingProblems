63eaf1d1d2d26296873d50cad74582736984e06a
John Medlock <john.medlock.dev@gmail.com>
2023-12-09 13:45:31 -0600

    Add project files.

---
+++ CodingProblems.Console.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CodingProblems.App.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ CodingProblems.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingProblems.App;
+
+internal class Program
+{
+    private static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
