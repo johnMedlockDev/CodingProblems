806f445e229f8b76dbc6657ff62a6cf9dc31b9b1
John Medlock <john.medlock.dev@gmail.com>
2023-12-25 04:45:23 -0600

    more h tests and funcs

--- CodeProblems.Tests/Edabit/HardTests.cs
+++ CodeProblems.Tests/Edabit/HardTests.cs
@@ -1,5 +1,4 @@
 ﻿using CodeProblems.Edabit;
-
 namespace CodeProblems.Tests.Edabit;
 
 [TestClass()]
@@ -200,6 +199,293 @@ public class HardTests
         var given = "CD5C5C";
         var expected = false;
         var actual = _sut.ValidHexCode(given);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/TAZywz6R2hu9tDQWc
+    [TestMethod()]
+    public void ConsecutiveNumbers_Test_1()
+    {
+        var given = new Int32[] { 5, 1, 4, 3, 2 };
+        var expected = true;
+        var actual = _sut.ConsecutiveNumbers(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void ConsecutiveNumbers_Test_2()
+    {
+        var given = new Int32[] { 5, 1, 4, 3, 2, 8 };
+        var expected = false;
+        var actual = _sut.ConsecutiveNumbers(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void ConsecutiveNumbers_Test_3()
+    {
+        var given = new Int32[] { 5, 6, 7, 8, 9, 9 };
+        var expected = false;
+        var actual = _sut.ConsecutiveNumbers(given);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/g88PKZrCY6sWPBva7
+    [TestMethod()]
+    public void TrackTheRobot_Test_1()
+    {
+        var given = new String[] { "right 10", "up 50", "left 30", "down 10" };
+        var expected = new Int32[] { -20, 40 };
+        var actual = _sut.TrackTheRobot(given);
+        _ = actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+    [TestMethod()]
+    public void TrackTheRobot_Test_2()
+    {
+        var given = Array.Empty<String>();
+        var expected = new Int32[] { 0, 0 };
+        var actual = _sut.TrackTheRobot(given);
+        _ = actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+    [TestMethod()]
+    public void TrackTheRobot_Test_()
+    {
+        var given = new String[] { "right 100", "right 100", "up 500", "up 10000" };
+        var expected = new Int32[] { 200, 10500 };
+        var actual = _sut.TrackTheRobot(given);
+        _ = actual.Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+    }
+    //https://edabit.com/challenge/X3btpQQEBeezX4jhK
+    [TestMethod()]
+    public void CupSwapping_Test_1()
+    {
+        var given = new String[] { "AB", "CA" };
+        var expected = "C";
+        var actual = _sut.CupSwapping(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void CupSwapping_Test_2()
+    {
+        var given = new String[] { "AC", "CA", "CA", "AC" };
+        var expected = "B";
+        var actual = _sut.CupSwapping(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void CupSwapping_Test_3()
+    {
+        var given = new String[] { "BA", "AC", "CA", "BC" };
+        var expected = "A";
+        var actual = _sut.CupSwapping(given);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/FKb8JY75nkaHz7B3F
+    [TestMethod()]
+    public void NextPrime_Test_1()
+    {
+        var given = 12;
+        var expected = 13;
+        var actual = _sut.NextPrime(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void NextPrime_Test_2()
+    {
+        var given = 24;
+        var expected = 29;
+        var actual = _sut.NextPrime(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void NextPrime_Test_3()
+    {
+        var given = 11;
+        var expected = 11;
+        var actual = _sut.NextPrime(given);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/wXCzoLtvvEEYBs3p9
+    [TestMethod()]
+    public void CountTheNumberOfDuplicateCharacters_Test_1()
+    {
+        var given = "abcde";
+        var expected = 0;
+        var actual = _sut.CountTheNumberOfDuplicateCharacters(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void CountTheNumberOfDuplicateCharacters_Test_2()
+    {
+        var given = "aabbcde";
+        var expected = 2;
+        var actual = _sut.CountTheNumberOfDuplicateCharacters(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void CountTheNumberOfDuplicateCharacters_Test_3()
+    {
+        var given = "Indivisibilities";
+        var expected = 2;
+        var actual = _sut.CountTheNumberOfDuplicateCharacters(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void CountTheNumberOfDuplicateCharacters_Test_4()
+    {
+        var given = "Aa";
+        var expected = 0;
+        var actual = _sut.CountTheNumberOfDuplicateCharacters(given);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/t6R99zCQ7nesR7Rdk
+    [TestMethod()]
+    public void AlmostPalindrome_Test_1()
+    {
+        var given = "abcdcbg";
+        var expected = true;
+        var actual = _sut.AlmostPalindrome(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void AlmostPalindrome_Test_2()
+    {
+        var given = "abccia";
+        var expected = true;
+        var actual = _sut.AlmostPalindrome(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void AlmostPalindrome_Test_3()
+    {
+        var given = "abcdaaa";
+        var expected = false;
+        var actual = _sut.AlmostPalindrome(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void AlmostPalindrome_Test_4()
+    {
+        var given = "1234312";
+        var expected = false;
+        var actual = _sut.AlmostPalindrome(given);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/y4esBva2cYph5QKg5
+    [TestMethod()]
+    public void AWeekLater_Test_1()
+    {
+        var given = "12/03/2020";
+        var expected = "19/03/2020";
+        var actual = _sut.AWeekLater(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void AWeekLater_Test_2()
+    {
+        var given = "21/12/1989";
+        var expected = "28/12/1989";
+        var actual = _sut.AWeekLater(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void AWeekLater_Test_3()
+    {
+        var given = "01/01/2000";
+        var expected = "08/01/2000";
+        var actual = _sut.AWeekLater(given);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/p39Q6JFuran7t5LpS
+    [TestMethod()]
+    public void DoAllBigramsExist_Test_1()
+    {
+        var givenA = new String[] { "at", "be", "th", "au" };
+        var givenB = new String[] { "beautiful", "the", "hat" };
+        var expected = true;
+        var actual = _sut.DoAllBigramsExist(givenA, givenB);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void DoAllBigramsExist_Test_2()
+    {
+        var givenA = new String[] { "ay", "be", "ta", "cu" };
+        var givenB = new String[] { "maybe", "beta", "abet", "course" };
+        var expected = false;
+        var actual = _sut.DoAllBigramsExist(givenA, givenB);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void DoAllBigramsExist_Test_3()
+    {
+
+        var givenA = new String[] { "th", "fo", "ma", "or" };
+        var givenB = new String[] { "the", "many", "for", "forest" };
+        var expected = true;
+        var actual = _sut.DoAllBigramsExist(givenA, givenB);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void DoAllBigramsExist_Test_4()
+    {
+        var givenA = new String[] { "oo", "mi", "ki", "la" };
+        var givenB = new String[] { "milk", "chocolate", "cooks" };
+        var expected = false;
+        var actual = _sut.DoAllBigramsExist(givenA, givenB);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/bqveyPRgcWZM7XzMQ
+    [TestMethod()]
+    public void ReverseCodingChallenge_Test_1()
+    {
+        var given = "A4B5C2";
+        var expected = "AAAABBBBBCC";
+        var actual = _sut.ReverseCodingChallenge(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void ReverseCodingChallenge_Test_2()
+    {
+        var given = "C2F1E5";
+        var expected = "CCFEEEEE";
+        var actual = _sut.ReverseCodingChallenge(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void ReverseCodingChallenge_Test_3()
+    {
+        var given = "T4S2V2";
+        var expected = "TTTTSSVV";
+        var actual = _sut.ReverseCodingChallenge(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void ReverseCodingChallenge_Test_4()
+    {
+        var given = "A1B2C3D4";
+        var expected = "ABBCCCDDDD";
+        var actual = _sut.ReverseCodingChallenge(given);
+        _ = actual.Should().Be(expected);
+    }
+    //https://edabit.com/challenge/C6wN5vGodWvWL7ZaK
+    [TestMethod()]
+    public void SockPairs_Test_1()
+    {
+        var given = "AA";
+        var expected = 1;
+        var actual = _sut.SockPairs(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void SockPairs_Test_2()
+    {
+        var given = "ABABC";
+        var expected = 2;
+        var actual = _sut.SockPairs(given);
+        _ = actual.Should().Be(expected);
+    }
+    [TestMethod()]
+    public void SockPairs_Test_3()
+    {
+        var given = "CABBACCC";
+        var expected = 4;
+        var actual = _sut.SockPairs(given);
         _ = actual.Should().Be(expected);
     }
 }
--- CodeProblems/Edabit/Hard.cs
+++ CodeProblems/Edabit/Hard.cs
@@ -1,5 +1,4 @@
-﻿
-namespace CodeProblems.Edabit;
+﻿namespace CodeProblems.Edabit;
 public class Hard
 {
     public String ReverseAndNot(Int32 number)
@@ -127,5 +126,268 @@ public class Hard
         }
 
         return true;
+    }
+
+    public Boolean ConsecutiveNumbers(Int32[] array)
+    {
+        var sortedArray = array.Order().ToArray();
+        var previous = sortedArray[0];
+
+        for (var i = 1; i < sortedArray.Length; i++)
+        {
+            var current = sortedArray[i];
+
+            var currentNotEqualPreviousPlusOne = !(current == previous + 1);
+
+            if (currentNotEqualPreviousPlusOne)
+            {
+                return false;
+            }
+
+            previous = current;
+        }
+
+        return true;
+    }
+
+    public Int32[] TrackTheRobot(String[] array)
+    {
+        var directionDict = new Dictionary<String, Int32>
+        {
+            { "right", 0 },
+            { "up", 0 },
+            { "left", 0 },
+            { "down", 0 }
+        };
+
+        foreach (var strpPair in array)
+        {
+            var pairArray = strpPair.Split(' ');
+            var direction = pairArray[0];
+            var directionValue = pairArray[1];
+
+            directionDict[direction] += Int32.Parse(directionValue);
+        }
+
+        var firstValue = directionDict["right"] - directionDict["left"];
+        var secondValue = directionDict["up"] - directionDict["down"];
+
+        return [firstValue, secondValue];
+    }
+
+    public String CupSwapping(String[] array)
+    {
+        var ballAddresses = new Boolean[] { false, true, false };
+
+        foreach (var swap in array)
+        {
+            if (swap.Equals("AB"))
+            {
+                var currentFirst = ballAddresses[0];
+                var currentSecond = ballAddresses[1];
+                ballAddresses[0] = currentSecond;
+                ballAddresses[1] = currentFirst;
+            }
+
+            if (swap.Equals("AC"))
+            {
+                var currentFirst = ballAddresses[0];
+                var currentSecond = ballAddresses[2];
+                ballAddresses[0] = currentSecond;
+                ballAddresses[2] = currentFirst;
+            }
+
+            if (swap.Equals("BC"))
+            {
+                var currentFirst = ballAddresses[1];
+                var currentSecond = ballAddresses[2];
+                ballAddresses[1] = currentSecond;
+                ballAddresses[2] = currentFirst;
+            }
+
+            if (swap.Equals("BA"))
+            {
+                var currentFirst = ballAddresses[1];
+                var currentSecond = ballAddresses[0];
+                ballAddresses[1] = currentSecond;
+                ballAddresses[0] = currentFirst;
+            }
+
+            if (swap.Equals("CA"))
+            {
+                var currentFirst = ballAddresses[2];
+                var currentSecond = ballAddresses[0];
+                ballAddresses[2] = currentSecond;
+                ballAddresses[0] = currentFirst;
+            }
+
+            if (swap.Equals("CB"))
+            {
+                var currentFirst = ballAddresses[2];
+                var currentSecond = ballAddresses[1];
+                ballAddresses[2] = currentSecond;
+                ballAddresses[1] = currentFirst;
+            }
+        }
+
+        return ballAddresses[0] ? "A" : ballAddresses[1] ? "B" : "C";
+    }
+
+    public Int32 NextPrime(Int32 number)
+    {
+        var currentNumber = number;
+        while (true)
+        {
+            var mulipleList = new List<Int32>();
+            for (var i = 1; i < currentNumber; i++)
+            {
+                if (currentNumber % i == 0)
+                {
+                    mulipleList.Add(i);
+                }
+            }
+
+            if (mulipleList.Count == 1)
+            {
+                return currentNumber;
+            }
+
+            currentNumber++;
+        }
+    }
+
+    public Int32 CountTheNumberOfDuplicateCharacters(String letters)
+    {
+        var letterCountDict = new Dictionary<Char, Int32>();
+        foreach (var letter in letters)
+        {
+            if (!letterCountDict.ContainsKey(letter))
+            {
+                letterCountDict[letter] = 1;
+            }
+            else
+            {
+                letterCountDict[letter] += 1;
+            }
+        }
+
+        var duplicateSum = 0;
+
+        foreach (var keyValuePair in letterCountDict)
+        {
+            if (keyValuePair.Value > 1)
+            {
+                duplicateSum++;
+            }
+        }
+
+        return duplicateSum;
+    }
+
+    public Boolean AlmostPalindrome(String characters)
+    {
+        var charactersReversed = new String(characters.ToCharArray().Reverse().ToArray());
+
+        var differences = 0;
+        for (var i = 0; i < characters.Length; i++)
+        {
+            var currentChar = characters[i];
+            var currentReversedChar = charactersReversed[i];
+
+            if (currentChar != currentReversedChar)
+            {
+                differences++;
+            }
+        }
+
+        return differences == 2;
+    }
+    public String AWeekLater(String date)
+    {
+        var dateNumArray = date.Split('/');
+        var day = Int32.Parse(dateNumArray[0]);
+        var month = Int32.Parse(dateNumArray[1]);
+        var year = Int32.Parse(dateNumArray[2]);
+        var currentDate = new DateOnly(year, month, day);
+
+        return currentDate.AddDays(7).ToString("dd/MM/yyyy");
+    }
+
+    public Boolean DoAllBigramsExist(String[] wordParts, String[] words)
+    {
+
+        var sb = new StringBuilder();
+        words.ToList().ForEach(word => sb.Append(word));
+        var arrayString = sb.ToString();
+
+        foreach (var word in wordParts)
+        {
+
+            if (!arrayString.Contains(word))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public String ReverseCodingChallenge(String charNumberPairs)
+    {
+        var charNumberDict = new Dictionary<Char, Int32>();
+
+        var charNumberPairArray = charNumberPairs.ToArray();
+        var currentChar = ' ';
+        for (var i = 0; i < charNumberPairArray.Length; i++)
+        {
+            if (i == 0 || i % 2 == 0)
+            {
+                currentChar = charNumberPairArray[i];
+                charNumberDict[currentChar] = 0;
+            }
+            else
+            {
+                charNumberDict[currentChar] = Int32.Parse(charNumberPairArray[i].ToString());
+            }
+        }
+
+        var sb = new StringBuilder();
+        foreach (var keyValuePair in charNumberDict)
+        {
+            var letter = keyValuePair.Key;
+            var value = keyValuePair.Value;
+            for (var i = 0; i < value; i++)
+            {
+                _ = sb.Append(letter);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public Int32 SockPairs(String letters)
+    {
+        var letterArray = letters.ToCharArray();
+        var charNumberDict = new Dictionary<Char, Int32>();
+
+        foreach (var letter in letterArray)
+        {
+            if (!charNumberDict.ContainsKey(letter))
+            {
+                charNumberDict[letter] = 1;
+            }
+            else
+            {
+                charNumberDict[letter] += 1;
+            }
+        }
+
+        var sumOfPairs = 0.0;
+        foreach (var keyValuePair in charNumberDict)
+        {
+            sumOfPairs += Math.Round(keyValuePair.Value / 2.0, 0, MidpointRounding.ToZero);
+        }
+
+        return (Int32) sumOfPairs;
     }
 }
